a334804d9b7782f89f292636d5c7b168ef3d8f7d
f1rset <f1rset.gaming@gmail.com>
2025-08-10 20:28:32 +0300

    fixed booting on orange pi zero 2

--- recipes-bsp/u-boot/files/aarch64/boot.cmd
+++ recipes-bsp/u-boot/files/aarch64/boot.cmd
@@ -6,6 +6,7 @@ if itest.b *0x28 == 0x02 ; then
 	rootdev=mmcblk1p2
 fi
 setenv bootargs console=${console} console=tty1 root=/dev/${rootdev} rootwait panic=10 ${extra}
+load mmc 0:1 ${fdt_addr_r} sun50i-h616-orangepi-zero2.dtb
 load mmc 0:1 ${fdt_addr_r} ${fdtfile}
 load mmc 0:1 ${kernel_addr_r} Image
 booti ${kernel_addr_r} - ${fdt_addr_r}
